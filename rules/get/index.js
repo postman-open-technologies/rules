1044017d7a933b463004b6db3fe916f5c6268973
Kin Lane <kinlane@gmail.com>
2022-10-28 15:16:06 -0700

    add base

---
+++ rules/get/index.js
@@ -0,0 +1,59 @@
+const vandium = require('vandium');
+const mysql  = require('mysql');
+
+exports.handler = vandium.generic()
+  .handler( (event, context, callback) => {
+
+    var connection = mysql.createConnection({
+    host     : process.env.host,
+    user     : process.env.user,
+    password : process.env.password,
+    database : process.env.database
+    });
+    
+    var search = 0;
+    if(event.search){
+      search = event.search;
+    }    
+    
+    var tags = 0;
+    if(event.tags){
+      tags = event.tags;
+    }   
+    
+    var rule = 0;
+    if(event.rule){
+      rule = event.rule;
+    }       
+    
+    var page = 0;
+    if(event.page){
+      page = event.page;
+    }
+    
+    var limit = 5;
+    if(event.limit){
+      limit = event.limit;
+    }   
+    if(limit > 25){
+      limit = 25;
+    }
+
+    var sql = "SELECT * FROM rules b WHERE id IS NOT NULL";
+    if(search != ''){
+       sql += " AND b.name LIKE '%" + search + "%'";
+    }
+    if(rule != ''){
+       sql += " AND id IN(SELECT rule_id FROM rules_rules WHERE rule_id IN(SELECT id FROM rules WHERE name = '" + rule + "'))";
+    }   
+    if(tags != ''){
+       sql += " AND id IN(SELECT rule_id FROM rules_tags WHERE tag_id IN(SELECT id FROM tags WHERE name IN ('" + tags.replace(",","','") + "')))";
+    }     
+    sql += " ORDER BY name";
+    sql += " LIMIT " + page + "," + limit;
+    connection.query(sql, function (error, results, fields) {
+
+    callback( null, results );
+
+  });
+});
---
+++ rules/role_id/tags/post/index.js
@@ -0,0 +1,44 @@
+const vandium = require('vandium');
+const mysql  = require('mysql');
+
+exports.handler = vandium.generic()
+  .handler( (event, context, callback) => {
+
+    var connection = mysql.createConnection({
+    host     : process.env.host,
+    user     : process.env.user,
+    password : process.env.password,
+    database : process.env.database
+    });
+
+
+    connection.connect(function(err) {
+      if (err) throw err;
+
+      var sql = "SELECT * FROM rules_tags WHERE rule_id = " + event.rule_id + " and tag_id = " + event.tag_id + " LIMIT 1";
+      connection.query(sql, function (err, result, fields) {
+        if (err) throw err;
+        if(result && result.length > 0){
+          callback( null, result[0] ); 
+          connection.end();
+        }
+        else{
+
+          var sql = "INSERT INTO rules_tags(rule_id,tag_id) VALUES(" + event.rule_id + ", " + event.tag_id + ")";
+        
+          connection.query(sql, function (error, results, fields) {
+
+            var inserted = {};
+            inserted.id = results.insertId;
+            inserted.rule_id = event.rule_id;
+            inserted.tag_id = event.tag_id;
+        
+            callback( null, inserted );
+            connection.end();
+      
+          });          
+
+        }
+      });
+    });
+});
